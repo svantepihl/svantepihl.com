b79a809778c9527f112c3e3c96e1e4ca482a7c30
Svante Pihl <hej@svantepihl.com>
2020-10-29 22:24:05 +0100

    missing semicolons

--- src/components/mainpage.vue
+++ src/components/mainpage.vue
@@ -3,7 +3,7 @@
       <div class=" ml-3 text-left">
         <h1 class="text-md md:text-6xl">svante pihl</h1>
         <a class="" href="mailto:hej@svantepihl.com">
-          <h3 class="xs:text-md md:text-2xl"> &lthej@svantepihl.com&gt</h3>
+          <h3 class="xs:text-md md:text-2xl"> &lt;hej@svantepihl.com&gt;</h3>
         </a>
       </div>
       <div class="bottom-0 absolute flex items-end justify-center bottom-10 animate-bounce animation-delay">
