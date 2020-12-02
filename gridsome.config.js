97c559bd25d8f5431f0f636988e780904d7b99a4
Svante Pihl <hej@svantepihl.com>
2020-12-02 10:57:00 +0100

    removed google analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -11,17 +11,6 @@ module.exports = {
   plugins: [
     {
       use: "gridsome-plugin-tailwindcss",
-      /**
-      * These are the default options. You don't need to set any options to get
-      * going. Seriously, you don't need to declare tailwind.config.js.
-
-      options: {
-        tailwindConfig: './tailwind.config.js',
-        presetEnvConfig: {},
-        shouldImport: true,
-        shouldTimeTravel: true
-      }
-      */
     },
   ],
 }
